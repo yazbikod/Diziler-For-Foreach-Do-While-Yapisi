3d1f58e2e92714d1ef61478850f9da266f6bf591
yazbikod <aslan_murathan@hotmail.com>
2022-10-25 02:23:48 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿int[] sayilar = new int[5];
+sayilar[0] = 10;
+sayilar[1] = 20;
+sayilar[2] = 30;
+sayilar[3] = 40;
+sayilar[4] = 50;
+
+
+for (int i = 0; i < sayilar.Length; i++)
+{
+    Console.WriteLine(sayilar[i]);
+}
+
+
+
+foreach (int x in sayilar)
+{
+    Console.WriteLine(x);
+}
+
+
+int j = 0;
+while (j < sayilar.Length)
+{
+    Console.WriteLine(sayilar[j]);
+    j++;
+}
+
+
+int s = 0;
+do
+{
+    Console.WriteLine(sayilar[s]);
+    s++;
+}
+while (s < sayilar.Length);
+
+
+//çift sayiları yazdır ve topla...
+int toplam = 0;
+foreach (int x in sayilar)
+{
+    if (x % 2 == 0)
+    {
+        toplam = x + toplam;
+        Console.WriteLine("Toplam : " + toplam);
+    }
+
+}
